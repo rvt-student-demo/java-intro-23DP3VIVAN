c0473494c4793f98cd10911b0f7338208a6cad58
Viktorija Ivanova <166131741+23DP3VIVAN@users.noreply.github.com>
2024-09-06 11:21:33 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,30 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi vardu:");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi uzvardu:");
+        String surname = scanner.nextLine();
+
+        System.out.println("Ievadi grupu:");
+        String group = scanner.nextLine();
+
+        System.out.println("Student :" + name + " " + surname + " " + group);
+
+        // Komentārs
+        // String name = "Vika";
+        // int age = 20;
+        // boolean isOnline = false;
+
+        // System.out.println(name);
 
     }
 
