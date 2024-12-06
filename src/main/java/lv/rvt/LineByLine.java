817282aa048e590fe89cb6d17aa06732cead2637
Viktorija Ivanova <166131741+23DP3VIVAN@users.noreply.github.com>
2024-12-06 08:14:52 +0000

    LineByLine

---
+++ src/main/java/lv/rvt/LineByLine.java
@@ -0,0 +1,13 @@
+String text = "once upon a time";
+String[] pieces = text.split(" ");
+
+System.out.println(pieces[0]);
+System.out.println(pieces[1]);
+System.out.println(pieces[2]);
+System.out.println(pieces[3]);
+
+System.out.println();
+
+for (int i = 0; i < pieces.length; i++) {
+    System.out.println(pieces[i]);
+}
