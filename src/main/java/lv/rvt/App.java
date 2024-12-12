bc36d10242c609bf1c4937c87d4c6703c702dea1
Viktorija Ivanova <166131741+23DP3VIVAN@users.noreply.github.com>
2024-12-12 10:21:11 +0000

    primitivie types

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,19 +3,19 @@ package lv.rvt;
 public class App
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception{
+        int x = 5;
+        int y = x;
+        int c = y;
 
-        // First bank transfer
-
-        Account acc1 = new Account("Matthews account", 1000.0);
-        Account acc2 = new Account("My account", 0.0);
-
-        acc1.withdraw(100.0);
-        acc2.deposit(100.0);
-
-        System.out.println(acc1);
-        System.out.println(acc2);
-        
+        modify(int value);
     }
+
+public static void modify(Person person){
+    person.setName("Moodified!");
+}
+
+public static void modify(int value){
+    value += 5;
+}
 }
